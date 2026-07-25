7cfd9428f5e5171bf7eb8c22ebaee8900d7c1e7d
Gavindu Sandeepa Samarawickrama <gavindusandeepa555@gmail.com>
2026-07-25 15:13:50 +0530

    Implement weather data fetching and display functionality

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Hi");
+const apiKey = "fa66d55bfb24497380e85722243011"
+
+const baseUrl = "http://api.weatherapi.com/v1"
+
+fetch(`${baseUrl}/current.json?key=${apiKey}&q=panadura`).then(res=>res.json()).then(data=>{
+    console.log(data);
+
+    document.getElementById("contentSection").innerHTML=`
+                <div>
+                <h1>${data.current.condition.text}</h1>
+                <h1>${data.location.name}</h1>
+                <img src="${data.current.condition.icon}" alt>
+                <p>${data.location.country}</p>
+                <p>${data.current.temp_c}</p>
+            </div>
+    `
+    
+})
+
+function btnSearchOnAction(){
+    let txtUserSearchValue = document.getElementById("txtSearch").value;
+    fetch(`${baseUrl}/current.json?key=${apiKey}&q=${txtUserSearchValue}`).then(res=>res.json()).then(data=>{
+    console.log(data);
+
+    document.getElementById("contentSection").innerHTML=`
+                <div>
+                <h1>${data.current.condition.text}</h1>
+                <h1>${data.location.name}</h1>
+                <img src="${data.current.condition.icon}" alt>
+                <p>${data.location.country}</p>
+                <p>${data.current.temp_c}</p>
+            </div>
+    `
+    
+})
+}
