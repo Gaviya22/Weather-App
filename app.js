05a852a01583a6e08e3af231f3f5d9f7b2b38744
Gavindu Sandeepa Samarawickrama <gavindusandeepa555@gmail.com>
2026-07-28 19:10:57 +0530

    New Commit

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function btnSearchOnAction(){
 navigator.geolocation.getCurrentPosition((position)=>{
     console.log(position);
     console.log(position.coords.latitude);
-    console.log(position.coords.longitud);
+    console.log(position.coords.longitude);
 
 });
 
